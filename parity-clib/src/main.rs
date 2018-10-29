8fb77b6e692b9a9f437f88b4337f6be498c2d6ee
Jakub Tomanik <jakub.tomanik@tigerspike.com>
2018-10-29 19:32:01 +0100

    updated example to reflect changes in lib

--- parity-clib/src/main.rs
+++ parity-clib/src/main.rs
@@ -9,7 +9,13 @@ use std::ffi::CStr;
 use std::io;
 use std::alloc::System;
 use std::os::raw::c_char;
+use std::slice;
+use std::str;
 
+#[cfg(feature = "malloc")]
+use std::alloc::System;
+
+#[cfg(feature = "malloc")]
 #[global_allocator]
 static A: System = System;
 
@@ -36,20 +42,32 @@ fn main() {
 				switch = false;
 			}
 			if srcunit.trim() == "a" {
-				let mut response: *mut c_char = ptr::null_mut();
-				let query3 = CString::new("{\"method\":\"eth_syncing\",\"params\":[],\"id\":1,\"jsonrpc\":\"2.0\"}").unwrap();
-				let ret3 = parity_rpc_ios_query(client, query3.as_ptr(), &mut response);
-				let response_string = CStr::from_ptr(response).to_string_lossy().into_owned();
-				parity_rpc_ios_release(response);
-				println!("returned {}", response_string);
+//				let mut response: *mut c_char = ptr::null_mut();
+//				let query3 = CString::new("{\"method\":\"eth_syncing\",\"params\":[],\"id\":1,\"jsonrpc\":\"2.0\"}").unwrap();
+//				let ret3 = parity_rpc_ios_query(client, query3.as_ptr(), &mut response);
+//				let response_string = CStr::from_ptr(response).to_string_lossy().into_owned();
+//				parity_rpc_ios_release(response);
+//				println!("returned {}", response_string);
 			}
 			if srcunit.trim() == "b" {
-				let mut response: *mut c_char = ptr::null_mut();
+//				let mut response: *mut c_char = ptr::null_mut();
 				let query3 = CString::new("{\"method\":\"web3_clientVersion\",\"params\":[],\"id\":1,\"jsonrpc\":\"2.0\"}").unwrap();
-				let ret3 = parity_rpc_ios_query(client, query3.as_ptr(), &mut response);
-				let response_string = CStr::from_ptr(response).to_string_lossy().into_owned();
-				parity_rpc_ios_release(response);
-				println!("returned {}", response_string);
+//				let ret3 = parity_rpc_ios_query(client, query3.as_ptr(), &mut response);
+				let mut response_pointer: *mut c_char = ptr::null_mut();
+				let mut response_length: usize = 0;
+				let ret3 = parity_rpc_ios_query(client, query3.as_ptr(), &mut response_pointer, &mut response_length);
+
+				if ret3 == 0 {
+					if response_length > 0 {
+//						let response_string = CStr::from_ptr(response_pointer).to_string_lossy().into_owned();
+						let string_bytes = slice::from_raw_parts(response_pointer as *mut u8, response_length);
+						let c_string = CString::new(string_bytes).unwrap().to_owned();
+						let string = c_string.to_str().to_owned().unwrap();
+						println!("returned :{}", string);
+					}
+
+					parity_rpc_ios_release(response_pointer);
+				}
 			}
 			srcunit = "".to_string();
 		}
